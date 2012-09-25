f4a251ad4e853a0c165b7af5274d77511caa73ad
Tobias Paczian <paczian@mcs.anl.gov>
2012-09-25 10:30:35 -0500

    finished basic set of documentation rearranged a function in stm for better organization (purely esthetical)

--- stm.js
+++ stm.js
@@ -9,20 +9,7 @@
     stm.DataRepositoryDefault = null;
     stm.DataRepositoriesCount = 0;
     
-    // get
-    stm.repository = function (name, value) {
-	if (typeof value !== 'undefined' &&
-	    typeof name  !== 'undefined' ) {
-	    return stm.DataRepositories[name] = value;
-	} else if (typeof name !== 'undefined') {
-	    return stm.DataRepositories[name];
-	} else {
-	    return stm.DataRepositories;
-	}
-    }
-    stm.repositories = function () { return stm.repository(); }
-    
-    // set up / reset the DataHandler, adding initial repositories
+    // set up / reset the stm, adding initial repositories
     stm.init = function (repo) {
 	stm.DataStore = [];
 	stm.TypeData = [];
@@ -36,6 +23,19 @@
 	    return stm.add_repository(repo);
 	}
     };
+
+    // get a repository, a repository attribute or the list of all repositories
+    stm.repository = function (name, value) {
+	if (typeof value !== 'undefined' &&
+	    typeof name  !== 'undefined' ) {
+	    return stm.DataRepositories[name] = value;
+	} else if (typeof name !== 'undefined') {
+	    return stm.DataRepositories[name];
+	} else {
+	    return stm.DataRepositories;
+	}
+    }
+    stm.repositories = function () { return stm.repository(); }
     
     // generic data loader
     // given a DOM id, interprets the innerHTML of the element as JSON data and loads it into the DataStore
@@ -171,7 +171,6 @@
 	} else {
 	    id = '';
 	}
-	var filter = params['filter'];
 	var options = params['options'];
 	
 	var query_params = "";
