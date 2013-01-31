19bc4f5f12081d4423630a842edfba35054970a9
Travis Harrison <guumby@gmail.com>
2013-01-30 22:12:33 -0600

    add function to execute cell and select cell

--- js/ipy.js
+++ js/ipy.js
@@ -4,49 +4,53 @@
 
     ipy.selected_cell_index = function () {
         return IPython.notebook.get_selected_index();
-    }
+    };
+
+    ipy.select_cell = function (index) {
+        IPython.notebook.select(index);
+    };
 
     ipy.read_cell = function (index) {
         if (index == undefined) {
             index = IPython.notebook.get_selected_index();
         }
-	return IPython.notebook.get_cell(index).get_text();
-    }
+	    return IPython.notebook.get_cell(index).get_text();
+    };
 
     ipy.write_cell = function (index, text) {
         if (index == undefined) {
             index = IPython.notebook.get_selected_index();
         }
-	if (IPython.notebook.get_cell(index) == null) {
-	    index = ipy.add_cell(null, null, 'bottom');
-	}
-	IPython.notebook.get_cell(index).set_text(text);
-    }
+	    if (IPython.notebook.get_cell(index) == null) {
+	        index = ipy.add_cell(null, null, 'bottom');
+	    }
+	    IPython.notebook.get_cell(index).set_text(text);
+    };
 
     ipy.append_to_cell = function (index, text) {
         if (index == undefined) {
             index = IPython.notebook.get_selected_index();
         }
-	IPython.notebook.get_cell(index).set_text(IPython.notebook.get_cell(index).get_text()+"\n"+text);
-    }
+	    IPython.notebook.get_cell(index).set_text(IPython.notebook.get_cell(index).get_text()+"\n"+text);
+    };
 
     ipy.createHTML = function () {
-	var cells = document.getElementsByClassName('output_subarea');
-	var html = "";
-	for (i=0;i<cells.length;i++) {
-	    html += cells[i].innerHTML;
-	}
-	stm.send_message(window.parent, { data: html, target: 'result'}, 'html');
-    }
+	    var cells = document.getElementsByClassName('output_subarea');
+	    var html = "";
+	    for (i=0;i<cells.length;i++) {
+	        html += cells[i].innerHTML;
+	    }
+	    stm.send_message(window.parent, { data: html, target: 'result'}, 'html');
+    };
 
     // if index undefined uses selected cells index (default of insert_cell_* functions)
     // default position is below given index or selected
     // return index of added cell
     ipy.add_cell = function (index, type, position) {
-	if (type == undefined) {
-	    type = 'code';
-	}
-	var cell = undefined;
+	    if (type == undefined) {
+	        type = 'code';
+	    }
+	    var cell = undefined;
         if (position == 'above') {
             cell = IPython.notebook.insert_cell_above(type, index);
         } else if (position == 'bottom') {
@@ -57,14 +61,22 @@
             cell = IPython.notebook.insert_cell_below(type, index);
         }
         return IPython.notebook.find_cell_index(cell);
-    }
+    };
+    
+    // execute cell of given index, default to current selected
+    ipy.execute_cell = function (index) {
+        if ((index !== undefined) && (index >= 0)) {
+            IPython.notebook.select(index);
+        }
+        IPython.execute_selected_cell();
+    };
     
     ipy.notebook_refresh = function () {
         IPython.notebook_list.load_list();
-    }
+    };
     
     ipy.notebook_save = function () {
         IPython.notebook.save_notebook();
-    }
+    };
     
 }).call(this);
