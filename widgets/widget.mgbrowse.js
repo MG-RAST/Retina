3ba6db0d23c91ed70c38d52183e8e827dc820fb3
Travis Harrison <guumby@gmail.com>
2013-10-21 11:59:19 -0500

    added more options to pass to mgbrowse for selectlist

--- widgets/widget.mgbrowse.js
+++ widgets/widget.mgbrowse.js
@@ -24,6 +24,7 @@
 		     "offset": 0,
 		     "total_count": 0,
 		     "query": {},
+		     "cv": ["project_id", "country", "sequence_type", "seq_method", "status"],
 		     "api_url": stm.Config.mgrast_api+'/metagenome?' };
     
     widget.display = function (wparams) {
@@ -51,8 +52,8 @@
 		    navigation_callback: widget.update,
 		    data: [],
 		    filter: result_columns,
-		    multiple: true,
-		    extra_wide: true,
+		    multiple: (wparams.multiple === false) ? false : true,
+		    extra_wide: wparams.wide || false,
 		    return_object: true,
 		    filter_attribute: 'name',
 		    value: "id"
@@ -79,90 +80,91 @@
     };
 
     widget.update = function (params) {
-	if (typeof params == 'string') {
-	    if (params == 'first') {
-		Retina.WidgetInstances.mgbrowse[1].state.offset = 0;
-	    }
-	    if (params == 'previous') {
-		Retina.WidgetInstances.mgbrowse[1].state.offset -= Retina.WidgetInstances.mgbrowse[1].state.limit;
-		if (Retina.WidgetInstances.mgbrowse[1].state.offset < 0) {
-		    Retina.WidgetInstances.mgbrowse[1].state.offset = 0;
-		}
-	    }
-	    if (params == 'next' || params == 'more') {
-		Retina.WidgetInstances.mgbrowse[1].state.offset += widget.state.limit;
-		if (params == 'more' && Retina.WidgetInstances.mgbrowse[1].state.total_count <= Retina.WidgetInstances.mgbrowse[1].state.limit) {
-		    return;
-		}
-	    }
-	    if (params == 'last') {
-		Retina.WidgetInstances.mgbrowse[1].state.offset = Retina.WidgetInstances.mgbrowse[1].state.numrows - Retina.WidgetInstances.mgbrowse[1].state.limit;
-		if (Retina.WidgetInstances.mgbrowse[1].state.offset < 0) {
-		    Retina.WidgetInstances.mgbrowse[1].state.offset = 0;
-		}
-	    }
-	} 
-	if (typeof params == 'object') {
-	    if (params.sort) {
-	        if (params.sort == 'default') {
-	            Retina.WidgetInstances.mgbrowse[1].state.sort = 'name';
-    		    Retina.WidgetInstances.mgbrowse[1].state.sortDir = 'asc';
-	        } else {
-		        Retina.WidgetInstances.mgbrowse[1].state.sort = params.sort;
-		        Retina.WidgetInstances.mgbrowse[1].state.sortDir = params.dir;
+	    if (typeof params == 'string') {
+	        if (params == 'first') {
+		        Retina.WidgetInstances.mgbrowse[1].state.offset = 0;
+	        }
+	        if (params == 'previous') {
+		        Retina.WidgetInstances.mgbrowse[1].state.offset -= Retina.WidgetInstances.mgbrowse[1].state.limit;
+		        if (Retina.WidgetInstances.mgbrowse[1].state.offset < 0) {
+		            Retina.WidgetInstances.mgbrowse[1].state.offset = 0;
+		        }
+	        }
+	        if (params == 'next' || params == 'more') {
+		        Retina.WidgetInstances.mgbrowse[1].state.offset += widget.state.limit;
+		        if (params == 'more' && Retina.WidgetInstances.mgbrowse[1].state.total_count <= Retina.WidgetInstances.mgbrowse[1].state.limit) {
+		            return;
+		        }
+	        }
+	        if (params == 'last') {
+		        Retina.WidgetInstances.mgbrowse[1].state.offset = Retina.WidgetInstances.mgbrowse[1].state.numrows - Retina.WidgetInstances.mgbrowse[1].state.limit;
+		        if (Retina.WidgetInstances.mgbrowse[1].state.offset < 0) {
+		            Retina.WidgetInstances.mgbrowse[1].state.offset = 0;
+		        }
+	        }
+	    } 
+	    if (typeof params == 'object') {
+	        if (params.sort) {
+	            if (params.sort == 'default') {
+	                Retina.WidgetInstances.mgbrowse[1].state.sort = 'name';
+    		        Retina.WidgetInstances.mgbrowse[1].state.sortDir = 'asc';
+	            } else {
+		            Retina.WidgetInstances.mgbrowse[1].state.sort = params.sort;
+		            Retina.WidgetInstances.mgbrowse[1].state.sortDir = params.dir;
+	            }
+	        }
+	        if (params.query) {
+		        if (Retina.WidgetInstances.mgbrowse[1].state.type == "listselect") {
+		            Retina.WidgetInstances.mgbrowse[1].state.offset = 0;
+		        }
+		        if (params.clear) {
+		            Retina.WidgetInstances.mgbrowse[1].state.query = {};
+		        }
+	            if (typeof params.query == 'object') {
+		            for (i=0;i<params.query.length;i++) {
+		                var searchword = (widget.state.cv.indexOf(params.query[i].field) > -1) ? params.query[i].searchword : "*"+params.query[i].searchword+"*";
+		                Retina.WidgetInstances.mgbrowse[1].state.query[params.query[i].field] = { "searchword": searchword, "comparison": params.query[i].comparison || "=" };
+		            }
+	            } else {
+	                Retina.WidgetInstances.mgbrowse[1].state.query = {};
+	            }
+	        }
+	        if (params.goto != null) {
+		        Retina.WidgetInstances.mgbrowse[1].state.offset = params.goto;
+	        }
+	        if (params.limit) {
+		        Retina.WidgetInstances.mgbrowse[1].state.limit = params.limit;
 	        }
 	    }
-	    if (params.query) {
-		if (Retina.WidgetInstances.mgbrowse[1].state.type == "listselect") {
-		    Retina.WidgetInstances.mgbrowse[1].state.offset = 0;
-		}
-		if (params.clear) {
-		    Retina.WidgetInstances.mgbrowse[1].state.query = {};
-		}
-	        if (typeof params.query == 'object') {
-		    for (i=0;i<params.query.length;i++) {
-		        Retina.WidgetInstances.mgbrowse[1].state.query[params.query[i].field] = { "searchword": "*"+params.query[i].searchword+"*", "comparison": params.query[i].comparison || "=" };
-		    }
-	        } else {
-	            Retina.WidgetInstances.mgbrowse[1].state.query = {};
+
+	    var query = "";
+	    for (i in Retina.WidgetInstances.mgbrowse[1].state.query) {
+	        if (Retina.WidgetInstances.mgbrowse[1].state.query.hasOwnProperty(i) && Retina.WidgetInstances.mgbrowse[1].state.query[i].searchword.length) {
+		        query += i + Retina.WidgetInstances.mgbrowse[1].state.query[i].comparison + Retina.WidgetInstances.mgbrowse[1].state.query[i].searchword + "&";
 	        }
 	    }
-	    if (params.goto != null) {
-		Retina.WidgetInstances.mgbrowse[1].state.offset = params.goto;
-	    }
-	    if (params.limit) {
-		Retina.WidgetInstances.mgbrowse[1].state.limit = params.limit;
-	    }
-	}
 
-	var query = "";
-	for (i in Retina.WidgetInstances.mgbrowse[1].state.query) {
-	    if (Retina.WidgetInstances.mgbrowse[1].state.query.hasOwnProperty(i) && Retina.WidgetInstances.mgbrowse[1].state.query[i].searchword.length) {
-		    query += i + Retina.WidgetInstances.mgbrowse[1].state.query[i].comparison + Retina.WidgetInstances.mgbrowse[1].state.query[i].searchword + "&";
-	    }
-	}
-
-	var url = Retina.WidgetInstances.mgbrowse[1].state.api_url + query + "order=" + Retina.WidgetInstances.mgbrowse[1].state.sort + "&direction=" + Retina.WidgetInstances.mgbrowse[1].state.sortDir + "&match=any&verbosity=mixs" + "&limit=" + Retina.WidgetInstances.mgbrowse[1].state.limit + "&offset=" + Retina.WidgetInstances.mgbrowse[1].state.offset;
-	var headers = stm.Authentication ? {'AUTH': stm.Authentication} : {};
+	    var url = Retina.WidgetInstances.mgbrowse[1].state.api_url + query + "order=" + Retina.WidgetInstances.mgbrowse[1].state.sort + "&direction=" + Retina.WidgetInstances.mgbrowse[1].state.sortDir + "&match=any&verbosity=mixs" + "&limit=" + Retina.WidgetInstances.mgbrowse[1].state.limit + "&offset=" + Retina.WidgetInstances.mgbrowse[1].state.offset;
+	    var headers = stm.Authentication ? {'AUTH': stm.Authentication} : {};
 	
-	jQuery.ajax({ url: url, headers: headers, dataType: "json", success: function(data) {
-	    if (Retina.WidgetInstances.mgbrowse[1].state.type == "table") {
-		    Retina.WidgetInstances.mgbrowse[1].result_table.settings.tdata = data.data;
-		    Retina.WidgetInstances.mgbrowse[1].result_table.settings.filter_changed = false;
-		    Retina.WidgetInstances.mgbrowse[1].result_table.settings.sorted = true;
-		    Retina.WidgetInstances.mgbrowse[1].result_table.settings.numrows = Retina.WidgetInstances.mgbrowse[1].state.numrows = data.total_count;
-		    Retina.WidgetInstances.mgbrowse[1].result_table.settings.offset = Retina.WidgetInstances.mgbrowse[1].state.offset;
-		    Retina.WidgetInstances.mgbrowse[1].result_table.render();
-	    } else {
-		    Retina.WidgetInstances.mgbrowse[1].state.total_count = data.total_count;
-		    if (typeof params == 'string' && params == "more") {
-		        Retina.WidgetInstances.mgbrowse[1].result_list.settings.data = Retina.WidgetInstances.mgbrowse[1].result_list.settings.data.concat(data.data);
-		    } else {
-		        Retina.WidgetInstances.mgbrowse[1].result_list.settings.data = data.data;
-		    }
-		    Retina.WidgetInstances.mgbrowse[1].result_list.render();
-	    }
-	}});
+	    jQuery.ajax({ url: url, headers: headers, dataType: "json", success: function(data) {
+	        if (Retina.WidgetInstances.mgbrowse[1].state.type == "table") {
+		        Retina.WidgetInstances.mgbrowse[1].result_table.settings.tdata = data.data;
+		        Retina.WidgetInstances.mgbrowse[1].result_table.settings.filter_changed = false;
+		        Retina.WidgetInstances.mgbrowse[1].result_table.settings.sorted = true;
+		        Retina.WidgetInstances.mgbrowse[1].result_table.settings.numrows = Retina.WidgetInstances.mgbrowse[1].state.numrows = data.total_count;
+		        Retina.WidgetInstances.mgbrowse[1].result_table.settings.offset = Retina.WidgetInstances.mgbrowse[1].state.offset;
+		        Retina.WidgetInstances.mgbrowse[1].result_table.render();
+	        } else {
+		        Retina.WidgetInstances.mgbrowse[1].state.total_count = data.total_count;
+		        if (typeof params == 'string' && params == "more") {
+		            Retina.WidgetInstances.mgbrowse[1].result_list.settings.data = Retina.WidgetInstances.mgbrowse[1].result_list.settings.data.concat(data.data);
+		        } else {
+		            Retina.WidgetInstances.mgbrowse[1].result_list.settings.data = data.data;
+		        }
+		        Retina.WidgetInstances.mgbrowse[1].result_list.render();
+	        }
+	    }});
     };
     
 })();
