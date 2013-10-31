18d148c8be5016f74749e4d809937b9915725792
Tobias Paczian <paczian@mcs.anl.gov>
2013-10-31 09:23:29 -0500

    updated invalid login handling to work with new globus version added onkeypress=enter -> click login button logic

--- widgets/widget.login.js
+++ widgets/widget.login.js
@@ -43,7 +43,7 @@
         <div id="failure"></div>\
         <table>\
           <tr><th style="vertical-align: top;padding-top: 5px;width: 100px;text-align: left;">login</th><td><input type="text" id="login"></td></tr>\
-          <tr><th style="vertical-align: top;padding-top: 5px;width: 100px;text-align: left;">password</th><td><input type="password" id="password"></td></tr>\
+          <tr><th style="vertical-align: top;padding-top: 5px;width: 100px;text-align: left;">password</th><td><input type="password" id="password" onkeypress="event = event || window.event;if(event.keyCode == 13) { Retina.WidgetInstances.login['+index+'].perform_login('+index+');}"></td></tr>\
         </table>\
       </div>\
       <div class="modal-footer">\
@@ -109,6 +109,14 @@
 									'token': null });
 		}
 	    }
+	}).fail(function() {
+	    document.getElementById('failure').innerHTML = '<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">&times;</button><strong>Error:</strong> Login failed.</div>';
+		if (Retina.WidgetInstances.login[index].callback && typeof(Retina.WidgetInstances.login[index].callback) == 'function') {
+		    Retina.WidgetInstances.login[index].callback.call({ 'action': 'login',
+									'result': 'failed',
+									'uname': null,
+									'token': null });
+		}
 	});
     };
     
