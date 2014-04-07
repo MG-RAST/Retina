78fa4e3b92ac25c6b4bcd937bf71af6a3fd9af47
Tobias Paczian <paczian@mcs.anl.gov>
2014-04-07 10:50:11 -0500

    updated for docker

--- widgets/widget.shockbrowse.js
+++ widgets/widget.shockbrowse.js
@@ -17,14 +17,19 @@
 	];
     };
 
-    widget.shock_url = "http://140.221.43.49:80/node?query&data_type=sequence";
+    widget.shock_base = "http://shock.metagenomics.anl.gov/node";
+    widget.shock_url = widget.shock_base + "?query&type=dockerimage";
     
     widget.display = function (wparams) {
         widget = this;
+	
+	if (Retina.WidgetInstances.hasOwnProperty('login') && Retina.WidgetInstances.login.length > 1) {
+	    Retina.WidgetInstances.login[1].callback = Retina.WidgetInstances.shockbrowse[1].update;
+	}
 
 	wparams.type || "table";
 
-	var result_columns = wparams.header || [ "created", "file_format", "id", "job_id", "kbase_id", "name", "project_id", "project_name" ];
+	var result_columns = wparams.header || [ "name", "base_image_tag", "docker version", "id", "temporary", "Arch", "GitCommit", "GoVersion", "KernelVersion", "Os" ];
 
 	var result_table_filter = wparams.filter;
 	if (result_table_filter == null) {
@@ -50,7 +55,6 @@
 		asynch_filter_attribute: 'name',
 		value: "id"
 	    });
-	    //widget.result_list.render();
 	    widget.result_list.update_data({},1);
 	} else {	
 	    widget.result_table = Retina.Renderer.create("table", {
@@ -62,8 +66,13 @@
 		synchronous: false,
 		query_type: 'prefix',
 		data_manipulation: widget.dataManipulation,
-//		invisible_columns: {0:1,2:1,6:1,7:1,8:1,11:1,12:1,15:1,16:1,17:1},
 		navigation_url: widget.shock_url,
+		invisible_columns: { 4: 1,
+				     5: 1,
+				     6: 1,
+				     7: 1,
+				     8: 1,
+				     9: 1 },
 		data: { data: [], header: result_columns }
 	    });
 	    widget.result_table.render();
@@ -71,10 +80,23 @@
 	}
     };
 
+    widget.update = function () {
+	Retina.WidgetInstances.shockbrowse[1].result_table.update({}, 1);
+    };
+
     widget.dataManipulation = function (data) {
 	var new_data = [];
 	for (var i=0;i<data.length;i++) {
-	    new_data.push(data[i].attributes);
+	    new_data.push({ "name": data[i].attributes.name, 
+			    "base_image_tag": data[i].attributes.base_image_tag,
+			    "docker version": data[i].attributes.docker_version.Version,
+			    "id": "<a href='"+Retina.WidgetInstances.shockbrowse[1].shock_base+"/"+data[i].id+"' target=_blank>"+data[i].id+"</a>",
+			    "temporary": data[i].attributes.temporary, 
+			    "Arch": data[i].attributes.docker_version.Arch, 
+			    "GitCommit": data[i].attributes.docker_version.GitCommit, 
+			    "GoVersion": data[i].attributes.docker_version.GoVersion,
+			    "KernelVersion": data[i].attributes.docker_version.KernelVersion, 
+			    "Os": data[i].attributes.docker_version.Os });
 	}
 	return new_data;
     };
