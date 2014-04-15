ac47432ffacdc5173ec3ec1a1514f0f433231c0f
Tobias Paczian <paczian@mcs.anl.gov>
2014-04-15 06:43:07 -0500

    completed documentation added callback function added popover tooltip added multiple options

--- renderers/renderer.tree.js
+++ renderers/renderer.tree.js
@@ -1,15 +1,8 @@
 /*
   Tree Renderer
 
-  Displays a searchable, collapsible tree view.
-
-  The selected node id is available through:
-
-  renderer.settings.selectedNode
-
-  The actual node can be retrieved via:
-
-  renderer.settings.data.nodes[renderer.settings.selectedNode]
+  Displays a searchable, collapsible tree view. The nodes displayed in this tree must be
+  an acyclic, directed, rooted graph.
 
   Options
 
@@ -34,6 +27,19 @@
   showSearchBar (BOOLEAN)
       Turn the search bar on / off. Default is true.
 
+  showSynonymsInDescription (BOOLEAN)
+      Turn the display of the synonym list in the hover over description on / off.
+      Default is true.
+
+  showTooltip (BOOLEAN)
+      Turn the display of the tooltip on / off. Default is true.
+
+  tooltipStyle (STRING)
+      Can be either "popover" or "plain", depending on how intrusive the tooltip should be. Default is popover.
+
+  callback (FUNCTION)
+      Function called when a node is selected. Passes the node object as a parameter.
+
   data (OBJECT)
       Content data structure. The top level must have the following attributes:
     - showRoot (BOOLEAN)
@@ -50,8 +56,20 @@
       The text displayed in the hover over of the node
       -childNodes (ARRAY)
       Array of node ids of child nodes of this node
-      -expanded (BOOLEAN - optional)
-      Sets the initial expansion of the node. Default is false. The root node cannot be collapsed.
+      - expanded (BOOLEAN - optional)
+      Sets the initial expansion of the node. Default is false. The root node cannot be collapsed if hidden.
+      - synonyms (ARRAY - optional)
+      List of strings of synonyms for this node. These will cause a hit in the search.
+
+  Functions
+
+  Note: All functions require the index of the renderer to be passed as the first parameter.
+
+  selectedNode (index)
+      Returns the currently selected node object or null if no node is selected.
+
+  goTo (index, nodeId)
+      Selects the node defined by nodeId, collapses all nodes and then expands all parents of the selected node.
 */
 (function () {
     var renderer = Retina.Renderer.extend({
@@ -67,10 +85,15 @@
 		'indent': 10,
 		'showCollapseAllButton': true,
 		'showExpandAllButton': true,
-		'showSearchBar': true
+		'showSearchBar': true,
+		'showSynonymsInDescription': true,
+		'callback': null,
+		'showTooltip': true,
+		'tooltipStyle': "popover"
 	    },
 	},
 
+	// example data to show the functionality of the tree renderer
 	exampleData: function () {
 	    return { "name": "testTree",
 		     "version": 1,
@@ -78,11 +101,12 @@
 		     "rootNode": "root",
 		     "nodes": { "root": { "id": "root",
 					  "label": "root",
-  	   	      			  "description": "cool node",
+					  "description": "cool node",
 					  "childNodes": [ "1", "2" ] },
 				"1": { "id": "1",
 				       "label": "node 1",
 				       "description": "first node",
+				       "synonyms": [ "Hans", "Dieter" ],
 				       "childNodes": [ "3", "4" ] },
 				"2": { "id": "2",
 				       "label": "node 2",
@@ -124,6 +148,7 @@
 	    renderer = this;
 	    var index = this.index;
 
+	    // tree node styles
 	    renderer.settings.target.innerHTML = "<style>\
 .tree-node {\
   cursor: pointer;\
@@ -148,6 +173,7 @@
 		renderer.setParentNode(index, renderer.settings.data.nodes[renderer.settings.data.rootNode], null);
 	    }
 
+	    // check if a collapse all button should be displayed
 	    if (renderer.settings.showCollapseAllButton) {
 		var cB = document.createElement('button');
 		cB.className = "btn btn-small";
@@ -161,6 +187,7 @@
 		renderer.settings.target.appendChild(cB);
 	    }
 
+	    // check if an expand all button should be displayed
 	    if (renderer.settings.showExpandAllButton) {
 		var cB = document.createElement('button');
 		cB.className = "btn btn-small";
@@ -174,48 +201,76 @@
 		renderer.settings.target.appendChild(cB);
 	    }
 
+	    // check if a search bar should be displayed
 	    if (renderer.settings.showSearchBar) {
+
+		// initialize the typeahead array
 		var typeAheadData = [];
+
+		// initialize the node label to node id lookup hash
 		var label2id = {};
+
+		// fill the typeahead array and the lookup hash
 		for (var i in renderer.settings.data.nodes) {
 		    if (renderer.settings.data.nodes.hasOwnProperty(i)) {
 			typeAheadData.push(renderer.settings.data.nodes[i].label);
 			label2id[renderer.settings.data.nodes[i].label] = i;
+			if (renderer.settings.data.nodes[i].hasOwnProperty('synonyms')) {
+			    for (var h=0; h<renderer.settings.data.nodes[i].synonyms.length; h++) {
+				typeAheadData.push(renderer.settings.data.nodes[i].synonyms[h]);
+				label2id[renderer.settings.data.nodes[i].synonyms[h]] = i;
+			    }
+			}
 		    }
 		}
+
+		// store the lookup hash in the settings
 		renderer.settings.data.label2id = label2id;
+
+		// create the searchbar div (input and button)
 		var sB = document.createElement('div');
 		sB.setAttribute('style', "float: left;");
 		sB.className = "input-append";
-		sB.innerHTML = "<input type='text' style='width: 144px;' id='tree_search_input_"+index+"' autocomplete='off'><button class='btn' onclick='Retina.RendererInstances.tree["+index+"].goTo("+index+");'>go</button>";
+		sB.innerHTML = "<input type='text' style='width: 144px; height: 16px; font-size: 11.9px;' id='tree_search_input_"+index+"' autocomplete='off'><button class='btn btn-small' onclick='Retina.RendererInstances.tree["+index+"].goTo("+index+");'>go</button>";
 		renderer.settings.target.appendChild(sB);
+
+		// add the typeahead to the input box
 		jQuery('#tree_search_input_'+index).typeahead({ source: typeAheadData });
 	    }
 
+	    // set the border style of the outer div
 	    renderer.settings.target.setAttribute('style', "border: 1px solid black; width: "+renderer.settings.width+"px; overflow: auto; height: "+renderer.settings.height+"px; padding: 5px; border-radius: 3px;");
 
+	    // create a space for the actual nodes
 	    renderer.settings.nodeSpace = document.createElement('div');
 	    renderer.settings.nodeSpace.setAttribute('style', "clear: both;");
 	    renderer.settings.target.appendChild(renderer.settings.nodeSpace);
-
+	    
+	    // call the node rendering function
 	    renderer.redraw(index);
 
 	    return renderer;
 	},
 
+	// draws the current state of the nodes
 	redraw: function (index) {
 	    var renderer = Retina.RendererInstances.tree[index];
 
+	    // check if we are displaying the root node
 	    var currIndent = -1;
 	    var hide = true;
 	    if (renderer.settings.data.showRoot) {
 		currIndent = 0;
 		hide = false;
+	    } else {
+		// the root node must always be expanded if hidden, otherwise there is nothing to be drawn
+		renderer.settings.data.nodes[renderer.settings.data.rootNode].expanded = true;
 	    }
 
+	    // empty the node space, we are redrawing all visible nodes every time
 	    renderer.settings.nodeSpace.innerHTML = "";
-	    renderer.settings.data.nodes[renderer.settings.data.rootNode].expanded = true;
 
+	    // initiate the first call to the recursive renderNode function
 	    renderer.renderNode( { "index": index,
 				   "node": renderer.settings.data.nodes[renderer.settings.data.rootNode],
 				   "indent": currIndent,
@@ -228,6 +283,7 @@
 	    var index = params.index;
 	    var renderer = Retina.RendererInstances.tree[index];
 	    
+	    // create the node div and check if it is the current selection
 	    var nodeDiv = document.createElement('div');
 	    if (renderer.settings.selectedNode && renderer.settings.selectedNode == params.node.id) {
 		nodeDiv.className = "tree-node-selected";
@@ -236,13 +292,21 @@
 	    }
 	    renderer.settings.nodeSpace.appendChild(nodeDiv);
 
+	    // if the node does not have the expanded property, set it to the default (false)
 	    if (! params.node.hasOwnProperty('expanded')) {
 		params.node.expanded = false;
 	    }
 
+	    // if this is the root node and supposed to be hidden, skip the rendering
 	    if (! params.hide) {
+
+		// set the indentation to the current level
 		nodeDiv.setAttribute('style', "padding-left: "+(renderer.settings.indent * params.indent)+"px;");
+
+		// initialize the node html
 		var html = "";
+
+		// if the node has children, it needs the expand / collapse graphic
 		if (params.node.childNodes.length) {
 		    if (params.node.expanded) {
 			html += "<span style='cursor: pointer; margin-right: 5px;' onclick='Retina.RendererInstances.tree["+index+"].settings.data.nodes[\""+params.node.id+"\"].expanded=false;Retina.RendererInstances.tree["+index+"].redraw("+index+");'>&dtrif;</span>";
@@ -250,15 +314,48 @@
 			html += "<span style='cursor: pointer; margin-right: 5px;' onclick='Retina.RendererInstances.tree["+index+"].settings.data.nodes[\""+params.node.id+"\"].expanded=true;Retina.RendererInstances.tree["+index+"].redraw("+index+");'>&rtrif;</span>";
 		    }
 		} else {
+
+		    // the node has no expand / collapse graphic, move it over so it alligns
 		    html += '<div style="float: left; height: 16px; width: 13px;"></div>';
 		}
-		html += "<span title='"+params.node.description+"' onclick='Retina.RendererInstances.tree["+index+"].settings.selectedNode=\""+params.node.id+"\";Retina.RendererInstances.tree["+index+"].redraw("+index+");'>"+params.node.label+"</span>";
+
+		// check for tooltip
+		var tooltip = "";
+
+		// if we want a tooltip, add it
+		if (renderer.settings.showTooltip) {
+
+		    // the base tooltip text is the description.
+		    var description = params.node.description;
+		    
+		    // check if we want the synonyms to be part of the tooltip
+		    if (renderer.settings.showSynonymsInDescription && params.node.hasOwnProperty('synonyms') && params.node.synonyms.length) {
+			description += "\n\nSynonyms:\n" + params.node.synonyms.join("\n");
+		    }	
+		    
+		    // check for tooltip style, either popover or plain
+		    if (renderer.settings.tooltipStyle == 'popover') {
+			description = description.replace(/\n/g, "<br>");
+			tooltip = "data-title='<span style=\"color: #333333;\">"+params.node.label+"</span>' data-html='true' data-content='<span style=\"color: #333333;\">"+description+"</span>' onmouseover='jQuery(this).popover(\"show\");' onmouseout='jQuery(this).popover(\"destroy\");' ";
+		    } else {
+			tooltip = "title='"+description+"' ";
+		    }
+		}
+
+		// create the node label
+		html += "<span "+tooltip+"onclick='Retina.RendererInstances.tree["+index+"].selectNode("+index+", \""+params.node.id+"\");'>"+params.node.label+"</span>";
 		
+		// add the node html to the node
 		nodeDiv.innerHTML = html;
 	    }
 	    
+	    // check if we have child nodes and are expanded, in that case, render the child nodes
 	    if (params.node.childNodes.length && params.node.expanded) {
+
+		// increment the indentation level
 		params.indent++;
+
+		// iterate over the child nodes and draw them with a recursive call to renderNode
 		for (var i=0; i<params.node.childNodes.length; i++) {
 		    renderer.renderNode( { "index": index,
 					   "node": renderer.settings.data.nodes[params.node.childNodes[i]],
@@ -267,6 +364,7 @@
 	    }
 	},
 
+	// collapse all nodes
 	collapseAll: function (index) {
 	    var renderer = Retina.RendererInstances.tree[index];
 
@@ -279,6 +377,7 @@
 	    renderer.redraw(index);
 	},
 
+	// expand all nodes
 	expandAll: function (index) {
 	    var renderer = Retina.RendererInstances.tree[index];
 
@@ -291,7 +390,8 @@
 	    renderer.renderer.redraw(index);
 	},
 
-	goTo: function (index) {
+	// go to a specific node, select it, collapse all nodes and expand the selected nodes parent nodes
+	goTo: function (index, nodeId) {
 	    var renderer = Retina.RendererInstances.tree[index];
 
 	    // collapse all nodes
@@ -302,7 +402,7 @@
 	    }
 	    
 	    // find the goto node
-	    var id = renderer.settings.data.label2id[document.getElementById('tree_search_input_'+index).value];
+	    var id = nodeId || renderer.settings.data.label2id[document.getElementById('tree_search_input_'+index).value];
 	    var node = renderer.settings.data.nodes[id];
 	    node.expanded = false;
 	    renderer.settings.selectedNode = id;
@@ -312,11 +412,18 @@
 		node = renderer.settings.data.nodes[node.parentNode];
 		node.expanded = true;
 	    }
+
+	    // check if someone wants to know about the selection
+	    if (typeof renderer.settings.callback == 'function') {
+		var node = renderer.settings.data.nodes[id];
+		renderer.settings.callback.call(null, node);
+	    }
 	    
 	    // rerender the tree
 	    renderer.redraw(index);
 	},
 
+	// if the node structure does not contain the parent references, create them here
 	setParentNode: function (index, node, parent) {
 	    var renderer = Retina.RendererInstances.tree[index];
 
@@ -326,6 +433,30 @@
 	    }
 
 	    return;
+	},
+
+	// select a specified node
+	selectNode: function (index, nodeId) {
+	    var renderer = Retina.RendererInstances.tree[index];
+
+	    // set the selected node to the passed nodeId
+	    renderer.settings.selectedNode = nodeId;
+
+	    // check if someone wants to know about the selection
+	    if (typeof renderer.settings.callback == 'function') {
+		var node = renderer.settings.data.nodes[nodeId];
+		renderer.settings.callback.call(null, node);
+	    }
+
+	    // redraw with the new settings
+	    renderer.redraw(index);
+	},
+
+	// returns the currently selected node
+	selectedNode: function (index) {
+	    var renderer = Retina.RendererInstances.tree[index];
+
+	    return renderer.settings.selectedNode ? renderer.settings.data.nodes[renderer.settings.selectedNode] : null;
 	}
 
     });
