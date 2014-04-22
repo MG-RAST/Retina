48bbaf6a160550a83f6d82adb8eaea947b33c012
Tobias Paczian <paczian@mcs.anl.gov>
2014-04-22 08:24:12 -0500

    removed sorting

--- widgets/widget.dockerimages.js
+++ widgets/widget.dockerimages.js
@@ -33,13 +33,18 @@
 	    Retina.WidgetInstances.login[1].callback = Retina.WidgetInstances.dockerimages[1].update;
 	}
 
-	var result_columns = [ "name", "base_image_tag", "docker version", "id", "temporary", "Arch", "GitCommit", "GoVersion", "KernelVersion", "Os" ];
+	var result_columns = [ "name", "namespace", "repository", "tag", "base_image_tag", "docker version", "shock node id", "docker image id", "temporary", "Arch", "GitCommit", "GoVersion", "KernelVersion", "Os" , "Size" ];
 
 	result_table_filter = {};
+	var disable_sort = {};
+	var nofilter = { 1: 1, 2: 1, 3: 1, 6: 1, 14: 1 };
 	for (var i=0;i<result_columns.length;i++) {
+	    disable_sort[i] = true;
+	    if (nofilter[i]) {
+		continue;
+	    }
 	    result_table_filter[i] = { "type": "text" };
 	}
-	
 	widget.result_table = Retina.Renderer.create("table", {
 	    target: widget.target,
 	    rows_per_page: 14,
@@ -47,13 +52,14 @@
 	    filter: result_table_filter,
 	    sort_autodetect: false,
 	    synchronous: false,
+	    disable_sort: disable_sort,
 	    query_type: 'prefix',
 	    headers: widget.headers,
 	    data_manipulation: widget.dataManipulation,
 	    asynch_column_mapping: { "name": "name",
 				     "base_image_tag": "base_image_tag",
 				     "docker version": "docker_version.Version",
-				     "id": "id",
+				     "docker image id": "id",
 				     "temporary": "temporary",
 				     "Arch": "docker_version.Arch",
 				     "GitCommit": "docker_version.GitCommit",
@@ -61,12 +67,15 @@
 				     "KernelVersion": "docker_version.KernelVersion",
 				     "Os": "docker_version.Os" },
 	    navigation_url: widget.shock_url,
-	    invisible_columns: { 4: 1,
-				 5: 1,
-				 6: 1,
-				 7: 1,
-				 8: 1,
-				 9: 1 },
+	    invisible_columns: { 0:  1,
+				 5:  1, //docker version
+				 7:  1,
+				 8:  1,
+				 9:  1,
+				 10: 1,
+				 11: 1,
+				 12: 1,
+				 13: 1 },
 	    data: { data: [], header: result_columns }
 	});
 	widget.result_table.render();
@@ -80,7 +89,16 @@
     widget.dataManipulation = function (data) {
 	var new_data = [];
 	for (var i=0;i<data.length;i++) {
-
+	    var namearray = data[i].attributes.name.split(":");
+	    var repository_tag = namearray[1] || "";
+	    var namespacearray = namearray[0].split("/");
+	    var namespace = "";
+	    var repository_name = namespacearray[0];
+	    if (namespacearray.length == 2) {
+		namespace=namespacearray[0];
+		repository_name=namespacearray[1];
+	    }
+	    
 	    var idfield = "<a href='"+Retina.WidgetInstances.dockerimages[1].shock_base+"/"+data[i].id+"' target=_blank title='no docker file available'>"+data[i].id+"</a>";
 	    if (data[i].attributes.dockerfile) {
 		idfield = "<a style='cursor: pointer;' onclick='Retina.WidgetInstances.dockerimages[1].tooltip(jQuery(this), \""+data[i].id+"\")'>"+data[i].id+"</a>";
@@ -90,16 +108,21 @@
 		stm.DataStore.dockerfile[data[i].id] = data[i].attributes.dockerfile;
 	    }
 	    
-	    new_data.push({ "name": data[i].attributes.name, 
+	    new_data.push({ "name": data[i].attributes.name,
+			    "namespace" : namespace,
+			    "repository" : repository_name,
+			    "tag" : repository_tag,
 			    "base_image_tag": data[i].attributes.base_image_tag,
 			    "docker version": data[i].attributes.docker_version.Version,
-			    "id": idfield,
-			    "temporary": data[i].attributes.temporary, 
-			    "Arch": data[i].attributes.docker_version.Arch, 
-			    "GitCommit": data[i].attributes.docker_version.GitCommit, 
+			    "shock node id": idfield,
+			    "docker image id" : data[i].attributes.id,
+			    "temporary": data[i].attributes.temporary,
+			    "Arch": data[i].attributes.docker_version.Arch,
+			    "GitCommit": data[i].attributes.docker_version.GitCommit,
 			    "GoVersion": data[i].attributes.docker_version.GoVersion,
 			    "KernelVersion": data[i].attributes.docker_version.KernelVersion,
-			    "Os": data[i].attributes.docker_version.Os });
+			    "Os": data[i].attributes.docker_version.Os,
+			    "Size": data[i].file.size });
 	}
 	return new_data;
     };
