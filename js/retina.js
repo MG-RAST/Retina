2fbeaa9a4bed35f6814c4ec18565c7285f76d5f3
Tobias Paczian <paczian@mcs.anl.gov>
2014-07-03 09:41:54 -0500

    ping function did not work

--- js/retina.js
+++ js/retina.js
@@ -351,37 +351,6 @@
 	return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
     };
 
-    Retina.ping = function (url, callback) {
-	if (!this.inUse) {
-            this.status = 'unchecked';
-            this.inUse = true;
-            this.callback = callback;
-            this.url = url;
-            var _that = this;
-            this.img = new Image();
-            this.img.onload = function () {
-		_that.inUse = false;
-		_that.callback('responded');
-		
-            };
-            this.img.onerror = function (e) {
-		if (_that.inUse) {
-                    _that.inUse = false;
-                    _that.callback('responded', e);
-		}
-		
-            };
-            this.start = new Date().getTime();
-            this.img.src = url;
-            this.timer = setTimeout(function () {
-		if (_that.inUse) {
-                    _that.inUse = false;
-                    _that.callback('timeout');
-		}
-            }, 1500);
-	}
-    };
-
     Number.prototype.formatString = function(c, d, t) {
 	var n = this, c = isNaN(c = Math.abs(c)) ? 0 : c, d = d == undefined ? "." : d, t = t == undefined ? "," : t, s = n < 0 ? "-" : "", i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
 	return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
