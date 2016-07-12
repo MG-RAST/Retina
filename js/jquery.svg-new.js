1b367a84d61ba28c8a657bde77fc5be9ebdd54bb
Tobias Paczian <paczian@mcs.anl.gov>
2016-07-12 08:26:30 -0500

    fixed svg tag

--- js/jquery.svg-new.js
+++ js/jquery.svg-new.js
@@ -98,6 +98,7 @@
 		if (!svg) {
 		    svg = document.createElementNS(this.svgNS, 'svg');
 		    svg.setAttribute('version', '1.1');
+		    svg.setAttribute('xmlns',"http://www.w3.org/2000/svg");
 		    if (container.clientWidth > 0) {
 			svg.setAttribute('width', container.clientWidth);
 		    }
